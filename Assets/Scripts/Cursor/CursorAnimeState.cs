b12f3be90d87424e7741c60bc62fa95928a55f9d
Kanbaru <lihaochen910@hotmail.com>
2017-09-05 12:15:42 +0800

    添加 光标控制脚本

---
+++ Assets/Scripts/Cursor/CursorAnimeState.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CursorAnimeState : MonoBehaviour {
+
+    public Sprite CursorSprite;
+    SpriteRenderer sRenderer;
+
+    float timer = 0;
+    public float TimeInterval = 0.5f;
+
+    // Use this for initialization
+    void Start()
+    {
+        sRenderer = GetComponentInChildren<SpriteRenderer>();
+
+    }
+
+    int index = 0;
+    void Update()
+    {
+
+        if (timer > TimeInterval)
+        {
+            if(index++ % 4 == 0 || index % 4 == 1 || index % 4 == 2)
+                sRenderer.sprite = CursorSprite;
+            else sRenderer.sprite = null;
+            timer = 0;
+        }
+        timer += Time.deltaTime;
+
+    }
+}
---
+++ Assets/Scripts/Cursor/CursorController.cs
@@ -0,0 +1,126 @@
+﻿using UnityEngine;
+using System.Collections;
+using FrameAnimeState;
+
+public class CursorController : MonoBehaviour {
+
+
+    public float unit = float.NaN;
+    public float CursorMoveTimeInterval = 0.1f;
+
+    float h, v;
+    bool isMoving = false;
+
+    s_RangeManager rManager;
+    void Start()
+    {
+        rManager = s_RangeManager._ins;
+    }
+
+    RaycastHit2D hit;
+    Actor currentActor;
+    void FixedUpdate()
+    {
+        if (CommandBox._ins.active)
+            return;
+        #region Move
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Vertical");
+        if (h == 1 && !isMoving)
+        {
+            isMoving = true;
+            iTween.MoveTo(gameObject, iTween.Hash("x", transform.position.x + unit,
+                "time", CursorMoveTimeInterval, "easytype", "linear", "oncomplete", "MovingEnd"));
+        }
+        if (h == -1 && !isMoving)
+        {
+            isMoving = true;
+            iTween.MoveTo(gameObject, iTween.Hash("x", transform.position.x - unit, "time", CursorMoveTimeInterval, "easytype", "linear", "oncomplete", "MovingEnd"));
+        }
+        if (v == 1 && !isMoving)
+        {
+            isMoving = true;
+            iTween.MoveTo(gameObject, iTween.Hash("y", transform.position.y + unit, "time", CursorMoveTimeInterval, "easytype", "linear", "oncomplete", "MovingEnd"));
+        }
+
+        if (v == -1 && !isMoving)
+        {
+            isMoving = true;
+            iTween.MoveTo(gameObject, iTween.Hash("y", transform.position.y - unit, "time", CursorMoveTimeInterval, "easytype", "linear", "oncomplete", "MovingEnd"));
+        }
+        #endregion
+    }//处理移动
+    void Update ()//处理当前range控件显示时的按键逻辑
+    {
+        if (!rManager.isShowing || CommandBox._ins.active)
+            return;
+        
+        if (Input.GetKeyDown(KeyCode.J))//Select
+        {
+            Ray ray = new Ray(transform.position, new Vector3(0, 0, 1));
+            hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+            if (hit)
+            {
+                //print(hit.transform.name);
+                if(hit.transform.tag == "InRange" && currentActor.tag != "Enemy")
+                {
+                    if (currentActor.isMoving)
+                        return;
+
+                    currentActor.MoveTo(hit.transform.position);
+                }
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.K))//Hide
+        {
+            print("取消移动，光标恢复到"+currentActor.name +currentActor.transform.position);
+
+            isMoving = true;
+            iTween.MoveTo(gameObject, iTween.Hash("x", currentActor.transform.position.x,"y", currentActor.transform.position.y, "time", CursorMoveTimeInterval, "easetype", "linear", "oncomplete", "MovingEnd"));
+
+            currentActor.HideMoveRange();
+            rManager.isShowing = false;
+        }
+
+
+    }
+    void LateUpdate()
+    {
+        
+        if (rManager.isShowing || CommandBox._ins.active)
+            return;
+        //通常状态下按键反应
+        #region Control
+        if (Input.GetKeyDown(KeyCode.J))//Select
+        {
+            Ray ray = new Ray(transform.position, new Vector3(0, 0, 1));
+            hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+            if (hit)
+            {
+                print(hit.transform.name);
+                currentActor = hit.transform.gameObject.GetComponent<Actor>();
+                if ((currentActor.transform.tag == "Ally" || currentActor.transform.tag == "Leader" || currentActor.transform.tag == "Enemy") && currentActor.currentState != ActorState.AwaitOrders)
+                    currentActor.ShowMoveRange();
+            }
+            else CommandBox._ins.Show(null, Command.RoundOver);
+        }
+        if (Input.GetKeyDown(KeyCode.K))//Cancel
+        {
+            if (currentActor.tag != "Leader" || currentActor.tag != "Ally")
+                return;
+            Vector3 Leader = GameObject.FindGameObjectWithTag("Leader").transform.localPosition;
+            //transform.position = new Vector3(Leader.x, Leader.y, transform.position.z);
+            isMoving = true;
+            iTween.MoveTo(gameObject, iTween.Hash("x", Leader.x, "y", Leader.y, "time", CursorMoveTimeInterval, "easetype", "spring", "oncomplete", "MovingEnd"));
+        }
+        #endregion
+    }
+
+    void MovingEnd()
+    {
+        isMoving = false;
+    }
+    
+}
