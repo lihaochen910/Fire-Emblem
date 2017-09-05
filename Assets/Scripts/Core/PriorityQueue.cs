2ee26b7091c6cde4a17a9fa9240cc508d11371d1
Kanbaru <lihaochen910@hotmail.com>
2017-09-05 12:17:59 +0800

    添加 .Net元组 添加 优先级队列

---
+++ Assets/Scripts/Core/PriorityQueue.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+
+namespace ebickle
+{
+    public class PriorityQueue<T>//优先队列
+    {
+        // I'm using an unsorted array for this example, but ideally this
+        // would be a binary heap. There's an open issue for adding a binary
+        // heap to the standard C# library: https://github.com/dotnet/corefx/issues/574
+        //
+        // Until then, find a binary heap class:
+        // * https://bitbucket.org/BlueRaja/high-speed-priority-queue-for-c/wiki/Home
+        // * http://visualstudiomagazine.com/articles/2012/11/01/priority-queues-with-c.aspx
+        // * http://xfleury.github.io/graphsearch.html
+        // * http://stackoverflow.com/questions/102398/priority-queue-in-net
+
+
+        //double or float
+        private List<Tuple<T, float>> elements = new List<Tuple<T, float>>();
+
+        public int Count
+        {
+            get { return elements.Count; }
+        }
+
+        public void Enqueue(T item, float priority)
+        {
+            elements.Add(Tuple.Create(item, priority));
+        }
+
+        public T Dequeue()
+        {
+            int bestIndex = 0;
+
+            for (int i = 0; i < elements.Count; i++)
+            {
+                if (elements[i].Item2 < elements[bestIndex].Item2)//priority值小，则先出队
+                {
+                    bestIndex = i;
+                }
+            }
+
+            T bestItem = elements[bestIndex].Item1;
+            elements.RemoveAt(bestIndex);
+            return bestItem;
+        }
+        public void Clear()
+        {
+            elements.Clear();
+        }
+    }
+}
---
+++ Assets/Scripts/Core/Tuple.cs
@@ -0,0 +1,31 @@
+﻿using System;
+/*
+    .NET Framework 4.7
+    File: system\tuple.cs
+    Project: ndp\clr\src\bcl\mscorlib.csproj (mscorlib)
+*/
+public static class Tuple
+{
+    public static Tuple<T1, T2> Create<T1, T2>(T1 item1, T2 item2)
+    {
+        return new Tuple<T1, T2>(item1, item2);
+    }
+}
+
+[Serializable]
+public class Tuple<T1, T2>
+{
+
+    private readonly T1 m_Item1;
+    private readonly T2 m_Item2;
+
+    public T1 Item1 { get { return m_Item1; } }
+    public T2 Item2 { get { return m_Item2; } }
+
+    public Tuple(T1 item1, T2 item2)
+    {
+        m_Item1 = item1;
+        m_Item2 = item2;
+    }
+
+}
